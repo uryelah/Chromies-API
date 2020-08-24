af960d4a159587b6220c5835f31e33efecd9ea41
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-08-24 15:59:36 -0400

    Added server connection method

---
+++ src/backend/server.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-unresolved */
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+const { connection } = mongoose;
+connection.once('open', () => {
+  console.log('MongoDB Database connection established successfully');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port: ', port);
+});
