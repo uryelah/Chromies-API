b24bc7a7d2697e5253c0eeee1d408ca8db56872e
Adebola <onedebos@gmail.com>
2020-08-24 23:48:03 +0100

    add skipper

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-unresolved */
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,15 +13,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const { connection } = mongoose;
-connection.once('open', () => {
-  console.log('MongoDB Database connection established successfully');
+connection.once("open", () => {
+  console.log("MongoDB Database connection established successfully");
 });
 
 app.listen(port, () => {
-  console.log('Server is running on port: ', port);
+  console.log("Server is running on port: ", port);
 });
