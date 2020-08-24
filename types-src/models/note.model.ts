f11a47d6ee0ec55031c5c280827d37ed85beb602
Adebola <onedebos@gmail.com>
2020-08-24 23:08:53 +0100

    add private option to config

--- types-src/models/note.model.ts
+++ types-src/models/note.model.ts
@@ -1,26 +1,31 @@
-"use strict";
 const mongoose = require("mongoose");
+
 const { Schema } = mongoose;
-const note = new Schema({
+const note = new Schema(
+  {
     pageLink: {
-        type: String,
-        trim: true,
-        required: "Page Link cannot be blank",
+      type: String,
+      trim: true,
+      required: "Page Link cannot be blank",
     },
     // store body as HTML string from FE
     body: { type: String, required: "Note body cannot be blank" },
     videoLink: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
     imgLink: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true,
     },
     private: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
-}, { collection: "notes" });
+  },
+  { collection: "notes" }
+);
+
 const Note = mongoose.model("note", note);
+
 module.exports = Note;
